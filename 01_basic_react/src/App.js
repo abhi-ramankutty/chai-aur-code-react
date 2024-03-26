4d5cbc879b747b0737e3aea084d67a1508b38a31
Abhilash  Ramankutty <abhi.ramankutty@gmail.com>
2024-03-26 12:22:43 +0530

    Basic react app created via classic method and via vite

---
+++ 01_basic_react/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div>
+            <h1>Chai aur Code - React | Abhilash</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ 01_vite_react/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <h1>Chai aur Code - React(vite) | Abhilash</h1>
+            <div>Test description</div>
+        </>
+    );
+}
+
+export default App;
