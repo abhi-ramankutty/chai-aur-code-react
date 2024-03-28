4c32143215a60b212be9545fe11cea32e8816ee7
Abhilash  Ramankutty <abhi.ramankutty@gmail.com>
2024-03-28 11:46:23 +0530

    Context api - discussion n project

---
+++ 08_mini_context/src/components/Login.jsx
@@ -0,0 +1,34 @@
+import React, {useState, useContext} from 'react';
+import UserContext from '../context/UserContext';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setUser({username, password});
+    };
+    const {setUser} = useContext(UserContext);
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <input
+                type='text'
+                placeholder='Username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type='password'
+                placeholder='Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    );
+}
+
+export default Login;
---
+++ 08_mini_context/src/context/UserContext.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserContext = React.createContext();
+
+export default UserContext;
+
+/**
+ * 1. Create a new js file (UserContext.js)
+ * 2. Import React from react and use it create context ``` const UserContext = React.createContext();```
+ * 3. Export the created context
+ */
+const notes = ''
